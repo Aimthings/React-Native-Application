23c9189cff78062e150e229a887df8a1a77db01d
Amit-Singh19 <amitsingh.tech19@gmail.com>
2021-12-30 23:41:16 +0530

    App Component Changes

--- App.js
+++ App.js
@@ -1,35 +1,10 @@
-import React from 'react';
-import {AppRegistry, Image, View} from 'react-native';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import {MaterialCommunityIcons } from '@expo/vector-icons';
+import React, { useState } from "react";
 
-import Screen from './app/components/Screen';
-import AppText from './app/components/AppText';
-import AppButton from './app/components/AppButton';
-import Card from './app/components/Card';
-import Icon from './app/components/Icon';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import MessageScreen from './app/screens/MessageScreen';
-import ListItem from './app/components/ListItem';
-import AccountsScreen from './app/screens/AccountsScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  return (
-  //  <Screen>
-  //    <ListItem 
-  //     title="My title" 
-  //     // subTitle="My subtitle" 
-  //     ImageComponent={<Icon name="email"/>}
-  //     />
-  //  </Screen>
-    // <AccountsScreen/>
-    <ListingsScreen/>
-  );
-} 
+  return <ListingEditScreen />;
+}
 
-
-//It is good practice to use stylesheet.create because validations happens for javacript when we do like this rather than writing style properties 
-//directly inside the braces
-
+//It is good practice to use stylesheet.create because validations happens for javacript when we do like this rather than writing style properties
+//directly inside the brace
